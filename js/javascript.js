708d98cab0bbedf815cec5dece4f8e52010570ba
Aritra96 <chattaraj.aritra@gmail.com>
2016-09-28 19:22:08 +0530

    Made the Batman Quiz - First Iteration
    Added a lot of stuff.

---
+++ js/javascript.js
@@ -0,0 +1,13 @@
+
+// function accept(){
+// var userInfo = prompt("What's your name?");
+//
+// alert("Hi "+userInfo+"! Welcome to my webpage!");
+// }
+
+// new Vue({
+//   el: '#app',
+//   data: {
+//     message: 'Hello World!'
+//   }
+// })
